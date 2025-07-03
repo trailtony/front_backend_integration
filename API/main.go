d666d457a4e632fba61449033bb17967d0473037
trailtony <still_running_uphill@protonmail.com>
2025-07-03 21:53:35 +0100

    Added to main.go the rest of the API methods (createBook, returnBook)

--- API/main.go
+++ API/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 
+	"errors"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,7 +35,7 @@ func bookById(c *gin.Context) {
 		return
 	}
 
-	c.IdentedJSON(http.StatusOK, book)
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func checkoutBook(c *gin.Context) {
@@ -57,4 +60,60 @@ func checkoutBook(c *gin.Context) {
 
 	book.Quantity -= 1
 	c.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	return nil, errors.New("book not found")
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "DELETE", "GET"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+	}))
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
+	router.Run("localhost:8080")
 }
--- Frontend/demo.js
+++ Frontend/demo.js
@@ -1,1 +1,5 @@
 import fetch from 'node-fetch';
+
+fetch("http://localhost:8080/books")
+.then((data => data.json())
+.then((data) => console.log(data)));
