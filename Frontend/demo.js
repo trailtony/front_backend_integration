db4f609c1c340f76189be127fac040b1d23a24c5
trailtony <still_running_uphill@protonmail.com>
2025-07-04 23:22:06 +0100

    Added frontend integration methods

--- Frontend/demo.js
+++ Frontend/demo.js
@@ -1,5 +1,32 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-fetch("http://localhost:8080/books")
-.then((data => data.json())
-.then((data) => console.log(data)));
+async function getData() {
+  const results = await fetch("http://localhost:8080/books")
+    .then((data) => data.json())
+    .then((data) => data);
+
+  for (const result of results) {
+    console.log(result);
+  }
+}
+
+async function createBook(id, author, title, quantity) {
+  const data = {
+    id,
+    author,
+    title,
+    quantity,
+  };
+  const result = await fetch("http://localhost:8080/books", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((data) => data.json())
+    .then((data) => data);
+  console.log(result);
+}
+
+createBook("4", "Test", "Tim", 1).then(() => getData());
